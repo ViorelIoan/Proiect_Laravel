cb511631c0d91b51cec13475d1373eda418fcf93
Viorel Pacurar <viorelioan.pacurar@live.com>
2022-01-26 22:06:05 +0200

    CRUD complete

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"
+    rel="stylesheet">
+    <title>Proiect Laravel</title>
+</head>
+<body>
+   <h1>Pacurar Viorel Ioan</h1> 
+<div class="container">
+    <br>
+    @yield('content')
+</div>
+
+</body>
+</html>
